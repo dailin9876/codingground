09b1160c482129cc3182eabfff63af53782a1a75
dailin9876 <dailin9876@gmail.com>
2016-06-13 14:21:45 +0800

    Created by Coding Ground Client

---
+++ New Project-20160613/main.d
@@ -0,0 +1,33 @@
+main: main.m Fraction.h \
+ /usr/GNUstep/System/Library/Headers/Foundation/NSObject.h \
+ /usr/GNUstep/System/Library/Headers/Foundation/NSObjCRuntime.h \
+ /usr/GNUstep/System/Library/Headers/GNUstepBase/GSVersionMacros.h \
+ /usr/GNUstep/System/Library/Headers/GNUstepBase/GSConfig.h \
+ /usr/GNUstep/System/Library/Headers/GNUstepBase/GSBlocks.h \
+ /usr/GNUstep/System/Library/Headers/GNUstepBase/GSObjCRuntime.h \
+ /usr/GNUstep/System/Library/Headers/Foundation/NSZone.h \
+ /usr/GNUstep/System/Library/Headers/GNUstepBase/GNUstep.h \
+ /usr/GNUstep/System/Library/Headers/Foundation/NSDate.h \
+ /usr/GNUstep/System/Library/Headers/GNUstepBase/NSObject+GNUstepBase.h
+
+Fraction.h:
+
+/usr/GNUstep/System/Library/Headers/Foundation/NSObject.h:
+
+/usr/GNUstep/System/Library/Headers/Foundation/NSObjCRuntime.h:
+
+/usr/GNUstep/System/Library/Headers/GNUstepBase/GSVersionMacros.h:
+
+/usr/GNUstep/System/Library/Headers/GNUstepBase/GSConfig.h:
+
+/usr/GNUstep/System/Library/Headers/GNUstepBase/GSBlocks.h:
+
+/usr/GNUstep/System/Library/Headers/GNUstepBase/GSObjCRuntime.h:
+
+/usr/GNUstep/System/Library/Headers/Foundation/NSZone.h:
+
+/usr/GNUstep/System/Library/Headers/GNUstepBase/GNUstep.h:
+
+/usr/GNUstep/System/Library/Headers/Foundation/NSDate.h:
+
+/usr/GNUstep/System/Library/Headers/GNUstepBase/NSObject+GNUstepBase.h:
