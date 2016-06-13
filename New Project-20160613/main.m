28c3cf7a48252a853da619bb0ca4449f6443bbd4
dailin9876 <dailin9876@gmail.com>
2016-06-13 16:18:20 +0800

    Update main.m

--- New Project-20160613/main.m
+++ New Project-20160613/main.m
@@ -1,10 +1,30 @@
-#import <Foundation/Foundation.h>
+#import <stdio.h>
+#import "Fraction.h"
 
 int main (int argc, const char * argv[])
 {
-   NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-   NSLog (@"hello world");
-   [pool drain];
+   Fraction *frac = [[Fraction alloc]init];
+   Fraction *frac2 = [[Fraction alloc]init];
+   
+   [frac setNumerator:1];
+   [frac setDenominator:2];
+   
+   [frac2 setNumerator:1 andDenominator:5];
+   
+   printf("The Fraction is:");
+   
+   [frac print];
+   
+   printf("\n");
+   
+   printf("Fraction 2 is:");
+   
+   [frac2 print];
+   
+   printf("\n");
+   
+   [frac release];
+   [frac2 release];
+   
    return 0;
 }
