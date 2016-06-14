3a6c835a59706f3dc28cd5f40cb9e462ed244a04
dailin9876 <dailin9876@gmail.com>
2016-06-14 16:32:21 +0800

    Created by Coding Ground Client

---
+++ New Project-20160613/Access.h
@@ -0,0 +1,14 @@
+#import <Foundation/NSObject>
+
+@interface Access NSObject
+{
+    @public 
+        int publicVar;
+    @private
+        int privateVar;
+        int privateVar2;
+    @protected
+        int protectedVar;
+}
+
+@end;
