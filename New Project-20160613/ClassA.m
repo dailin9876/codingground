eff7d54ec3535b9d692292af1f6af01d33f0d605
dailin9876 <dailin9876@gmail.com>
2016-06-14 16:54:31 +0800

    Created by Coding Ground Client

---
+++ New Project-20160613/ClassA.m
@@ -0,0 +1,22 @@
+#import "ClassA.h"
+
+@implementation ClassA
+
+-(id) init
+{
+    self = [super init];
+    count++;
+    return self;
+}
+
++(int) initCount
+{
+    return count;
+}
+
++(void) initialize
+{
+    count = 0;
+}
+
+@end
